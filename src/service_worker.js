c614fefa0b56b5a9e226de3bfd20c836cd977bbe
Harsh Kapadia <harshgkapadia@gmail.com>
2021-02-09 12:17:37 +0530

    :sparkles: feat: Add PWA and CI/CD features

---
+++ src/service_worker.js
@@ -0,0 +1,80 @@
+const cacheName = "v1";
+const cacheAssets = [
+	"./",
+	"./index.html",
+	"./static/css/pwa_install.css",
+	"./static/js/main.js",
+	"./static/img/icon.svg",
+	"./static/img/clear_cross.svg"
+];
+
+self.addEventListener
+(
+	"install",
+	(e) =>
+	{
+		// Cache files
+		e.waitUntil
+		(
+			caches.open(cacheName)
+				.then((cache) => cache.addAll(cacheAssets))
+				.then(() => self.skipWaiting())
+				.catch((err) => console.error(`Cache error: ${err}`))
+		);
+	}
+);
+
+self.addEventListener
+(
+	"activate",
+	(e) =>
+	{
+		e.waitUntil
+		(
+			// Delete any previous cache
+			caches.keys()
+			.then
+			(
+				(cacheKeys) =>
+				{
+					return Promise.all
+					(
+						cacheKeys.map
+						(
+							(cacheKey) =>
+							{
+								if(cacheKey !== cacheName)
+									return caches.delete(cacheKey);
+							}
+						)
+					);
+				}
+			)
+		);
+	}
+);
+
+self.addEventListener
+(
+	"fetch",
+	(e) =>
+	{
+		e.respondWith
+		(
+			fetch(e.request)
+			.then
+			(
+				(response) =>
+				{
+					const resClone = response.clone();
+
+					// Add response to cache
+					if(e.request.url.indexOf('http') === 0)
+						caches.open(cacheName).then((cache) => cache.put(e.request, resClone));
+
+					return response;
+				}
+			).catch((err) => caches.match(e.request).then((response) => response))
+		);
+	}
+);
